c94df99cc5e074c3ab460dcdb9e46c8630519dc4
Jonatan Santos <jonatan.zarowny@gmail.com>
2023-09-12 22:35:07 -0300

    Project scaffold
    mix new webmania_nfe

---
+++ lib/webmania_nfe.ex
@@ -0,0 +1,18 @@
+defmodule WebmaniaNfe do
+  @moduledoc """
+  Documentation for `WebmaniaNfe`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WebmaniaNfe.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/webmania_nfe_test.exs
@@ -0,0 +1,8 @@
+defmodule WebmaniaNfeTest do
+  use ExUnit.Case
+  doctest WebmaniaNfe
+
+  test "greets the world" do
+    assert WebmaniaNfe.hello() == :world
+  end
+end
